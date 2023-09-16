02cfdcdf1182afadcd33dc2de12a25c58d372860
romulodelmondes <romulodelmondes@gmail.com>
2023-09-16 17:15:28 -0300

    Atualização 16/09/2023 17:15

--- Documents/NetBeansProjects/sistemasoo/src/main/java/capitulo3/exemplos/exemplo1/OlaMundo.java
+++ Documents/NetBeansProjects/sistemasoo/src/main/java/capitulo3/exemplos/exemplo1/OlaMundo.java
@@ -3,6 +3,6 @@ package capitulo3.exemplos.exemplo1;
 public class OlaMundo {
 
     public static void main(String[] args) {
-        System.out.println("Rômulo da Costa Delmondes");
+        System.out.println("Olá Mundo!");
     }
 }
